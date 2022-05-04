d4c4b8e3294ab741cb89ddd653eb0e82e8e84a9d
elisee Desmarest <elisee.desmarest@gmail.com>
2022-05-04 16:14:26 +0200

    category update and delete

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\CategoryType;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -15,14 +17,56 @@ class HomeController extends AbstractController
      */
     public function index(ManagerRegistry $doctrine): Response
     {
-        $category = new Category;
-
         $em = $doctrine->getManager();
         $categories = $em->getRepository(Category::class)->findAll();
 
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
             'categories' => $categories
+        ]);
+    }
+
+    /**
+     * @Route("/category/delete/{id}", name="category_delete")
+     */
+    public function delete(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $em = $doctrine->getManager();
+        $category = $em->getRepository(Category::class)->findOneBy(['id' => $request->get('id')]);
+
+        if ($category) {
+            $em->getRepository(Category::class)->remove($category);
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('category/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/category/update/{id}", name="category_update")
+     */
+    public function update(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $em = $doctrine->getManager();
+        $category = $em->getRepository(Category::class)->findOneBy(['id' => $request->get('id')]);
+
+        if ($category) {
+            $form_update = $this->createForm(CategoryType::class, $category);
+            $form_update->handleRequest($request);
+
+            if ($form_update->isSubmitted() && $form_update->isValid()) {
+                $em = $doctrine->getManager();
+                $em->persist($form_update->getData());
+                $em->flush();
+
+                return $this->redirectToRoute('home');
+            }
+        } else {
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('category/edit.html.twig', [
+            'form_update' => $form_update->createView()
         ]);
     }
 }
