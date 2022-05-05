e16ecc2c7dca9b6bd3d2b45be4adb06da90d284d
elisee Desmarest <elisee.desmarest@gmail.com>
2022-05-05 14:07:36 +0200

    CRUD post

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\Post;
-use App\Entity\Category;
+use App\Form\PostType;
 use App\Form\CategoryType;
+use App\Repository\PostRepository;
+use App\Repository\CategoryRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,52 +17,43 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(PostRepository $postRepository, CategoryRepository $categoryRepository): Response
     {
-        $em = $doctrine->getManager();
-        $categories = $em->getRepository(Category::class)->findAll();
-
-        $em = $doctrine->getManager();
-        $posts = $em->getRepository(Post::class)->findAll();
-
         return $this->render('home/index.html.twig', [
-            'categories' => $categories,
-            'posts' => $posts
+            'categories' => $categoryRepository->findAll(),
+            'posts' => $postRepository->findAll()
         ]);
     }
 
     /**
      * @Route("/category/delete/{id}", name="category_delete")
      */
-    public function delete(Request $request, ManagerRegistry $doctrine): Response
+    public function delete(Request $request, CategoryRepository $categoryRepository): Response
     {
-        $em = $doctrine->getManager();
-        $category = $em->getRepository(Category::class)->findOneBy(['id' => $request->get('id')]);
+        $category = $categoryRepository->findOneBy(['id' => $request->get('id')]);
 
         if ($category) {
-            $em->getRepository(Category::class)->remove($category);
+            $categoryRepository->remove($category);
 
             return $this->redirectToRoute('home');
         }
-
-        return $this->render('category/index.html.twig', []);
     }
 
     /**
      * @Route("/category/update/{id}", name="category_update")
      */
-    public function update(Request $request, ManagerRegistry $doctrine): Response
+    public function update(Request $request, ManagerRegistry $doctrine, CategoryRepository $categoryRepository): Response
     {
         $em = $doctrine->getManager();
-        $category = $em->getRepository(Category::class)->findOneBy(['id' => $request->get('id')]);
+        $category = $categoryRepository->findOneBy(['id' => $request->get('id')]);
 
         if ($category) {
-            $form_update = $this->createForm(CategoryType::class, $category);
-            $form_update->handleRequest($request);
+            $formUpdate = $this->createForm(CategoryType::class, $category);
+            $formUpdate->handleRequest($request);
 
-            if ($form_update->isSubmitted() && $form_update->isValid()) {
+            if ($formUpdate->isSubmitted() && $formUpdate->isValid()) {
                 $em = $doctrine->getManager();
-                $em->persist($form_update->getData());
+                $em->persist($formUpdate->getData());
                 $em->flush();
 
                 return $this->redirectToRoute('home');
@@ -71,7 +63,49 @@ class HomeController extends AbstractController
         }
 
         return $this->render('category/edit.html.twig', [
-            'form_update' => $form_update->createView()
+            'formUpdate' => $formUpdate->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/post/delete/{id}", name="post_delete")
+     */
+    public function deletePost(Request $request, PostRepository $postRepository): Response
+    {
+        $post = $postRepository->findOneBy(['id' => $request->get('id')]);
+
+        if ($post) {
+            $postRepository->remove($post);
+
+            return $this->redirectToRoute('home');
+        }
+    }
+
+    /**
+     * @Route("/post/update/{id}", name="post_update")
+     */
+    public function updatePost(Request $request, ManagerRegistry $doctrine, PostRepository $postRepository): Response
+    {
+        $em = $doctrine->getManager();
+        $post = $postRepository->findOneBy(['id' => $request->get('id')]);
+
+        if ($post) {
+            $formUpdatePost = $this->createForm(PostType::class, $post);
+            $formUpdatePost->handleRequest($request);
+
+            if ($formUpdatePost->isSubmitted() && $formUpdatePost->isValid()) {
+                $em = $doctrine->getManager();
+                $em->persist($formUpdatePost->getData());
+                $em->flush();
+
+                return $this->redirectToRoute('home');
+            }
+        } else {
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('post/edit.html.twig', [
+            'formUpdatePost' => $formUpdatePost->createView()
         ]);
     }
 }
